d48a81808a8e2d4f5e6a8975eb89e045e9678ba5
devwood <tunglersloy.c@gmail.com>
2017-10-09 16:37:30 +0700

    e

--- bot.php
+++ bot.php
@@ -1,25 +1,23 @@
 <?php
 
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
-
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = 'bb695d4886535ef7ce2d91adea52c544';
-$access_token  = 'NENB7H4HyQxHVCl8OJ94uvbss5SOxzlTNYPk02k+BuzBjG3OczD2x7rDlXgfjR9VAr3FJqIdK8GoKzbsNAiDfQ6NWVPy+JCYNhjZ/5zyt2H+4RHcDvtHNE5JDS27CRHsAyS5El5uVBXYds2s76MeRAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '551ec4feee0.....43cff0';
+$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
 		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
 
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
 
 }
