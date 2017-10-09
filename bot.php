430b8a2a7ded67455b2b8a8288a23b33e197fcc2
devwood <tunglersloy.c@gmail.com>
2017-10-09 22:36:58 +0700

    j

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => '500'
+				'text' => '500 '.$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
