7ff0f89f8f7e4c82fe4791a09cd48eab73237234
devwood <tunglersloy.c@gmail.com>
2017-10-09 22:03:32 +0700

    7

--- bot.php
+++ bot.php
@@ -1,6 +1,9 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+
+if(1=0)
+{
+	include ('line-bot-api/php/line-bot.php');
 
 $channelSecret = '551ec4feee0.....43cff0';
 $access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
@@ -20,5 +23,8 @@ if (!empty($bot->isEvents)) {
     //echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
 	echo 'Error!';
     exit();
+}
+
+echo 'Error!';
 
 }
