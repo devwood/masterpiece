2611bd55cf05de7ef94268c52ef09ac726b2f64e
devwood <tunglersloy.c@gmail.com>
2017-10-10 13:11:26 +0700

    s

--- bot.php
+++ bot.php
@@ -16,10 +16,16 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+
+			$userId = $event['source']['userId'];
+			$id = $event['message']['id'];
+
+
+
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Test2 '.$text.' Reply='.$replyToken
+				'text' => 'Test2 '.$text.' Reply='.$replyToken.' user='.$userId
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
