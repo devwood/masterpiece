a46a1d734fe01d7c588d9e775232072c4b201ab1
devwood <tunglersloy.c@gmail.com>
2017-10-11 11:22:29 +0700

    9

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Test8 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id
+				'text' => 'Test9 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
