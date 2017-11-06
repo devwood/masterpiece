f188665cb4a758bf562301cadf29d61c420bf3a8
devwood <tunglersloy.c@gmail.com>
2017-11-06 11:18:10 +0700

    20

--- bot.php
+++ bot.php
@@ -3,10 +3,7 @@ $access_token = 'NENB7H4HyQxHVCl8OJ94uvbss5SOxzlTNYPk02k+BuzBjG3OczD2x7rDlXgfjR9
 //Connect DB
 $dbconn = pg_connect("host=ec2-107-22-252-91.compute-1.amazonaws.com port=5432 dbname=d1t089mnl00iir user=feajajzganbfiq password=57ba34efa8018b168b1edbdd5849b55f67c2a8a1f48e644a1e1fc6e951d9517a");
 //$result = pg_query($dbconn, "SELECT * FROM KNOW");
-$know = 'SELECT * FROM "KNOW" WHERE "FACTOR" like ';
-$know = $know."'%001%'";
-$result = pg_exec($dbconn, $know );
-$numrows = pg_numrows($result);
+
 //var_dump(pg_fetch_all($result));
 //connect to a database named "mary" on the host "sheep" with a username and password
 
@@ -24,6 +21,14 @@ if (!is_null($events['events'])) {
 		{
 			// Get text sent
 			$text = $event['message']['text'];
+			
+			$know = 'SELECT * FROM "KNOW" WHERE "FACTOR" like ';
+			$know = $know."'%".$text."%'";
+			$result = pg_exec($dbconn, $know );
+			$numrows = pg_numrows($result);
+			
+			
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -36,7 +41,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Test19 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id.'rows = '.$numrows
+				//'text' => 'Test19 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id.'rows = '.$numrows
+				'text' => 'Test20 '.$text.' query='.$know.'rows = '.$numrows
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
