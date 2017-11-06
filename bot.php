b89f77bdccf18811ba149f031c1dd174ea35c93b
devwood <tunglersloy.c@gmail.com>
2017-11-06 09:14:52 +0700

    exec

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 $access_token = 'NENB7H4HyQxHVCl8OJ94uvbss5SOxzlTNYPk02k+BuzBjG3OczD2x7rDlXgfjR9VAr3FJqIdK8GoKzbsNAiDfQ6NWVPy+JCYNhjZ/5zyt2H+4RHcDvtHNE5JDS27CRHsAyS5El5uVBXYds2s76MeRAdB04t89/1O/w1cDnyilFU=';
 //Connect DB
 $dbconn = pg_connect("host=ec2-107-22-252-91.compute-1.amazonaws.com port=5432 dbname=d1t089mnl00iir user=feajajzganbfiq password=57ba34efa8018b168b1edbdd5849b55f67c2a8a1f48e644a1e1fc6e951d9517a");
-$result = pg_query($dbconn, "SELECT * FROM KNOW");
-//$result = pg_exec($dbconn, "SELECT * FROM KNOW");
+//$result = pg_query($dbconn, "SELECT * FROM KNOW");
+$result = pg_exec($dbconn, "SELECT * FROM KNOW");
 $numrows = pg_numrows($result);
 //var_dump(pg_fetch_all($result));
 //connect to a database named "mary" on the host "sheep" with a username and password
@@ -34,7 +34,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Test12 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id.'rows = '.$numrows
+				'text' => 'Test13 '.$text.' Reply='.$replyToken.' user='.$userId.' id='.$id.'rows = '.$numrows
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
