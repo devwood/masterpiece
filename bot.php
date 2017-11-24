73ffafc7f0da192671007745b94a61c10084552c
devwood <tunglersloy.c@gmail.com>
2017-11-24 09:54:29 +0700

    29

--- bot.php
+++ bot.php
@@ -292,34 +292,34 @@ function _resultMSG($text, $dbconn, $event);
 		$numrows = 1;
 	}
 	
-	{
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/reply';
-		/*
-		$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages, $messages],
-		];
-		*/
-		$data = [
-			'replyToken' => $replyToken,
-			'messages' => $messagesX,
-		];
+	// {
+		// // Make a POST Request to Messaging API to reply to sender
+		// $url = 'https://api.line.me/v2/bot/message/reply';
+		// /*
+		// $data = [
+			// 'replyToken' => $replyToken,
+			// 'messages' => [$messages, $messages],
+		// ];
+		// */
+		// $data = [
+			// 'replyToken' => $replyToken,
+			// 'messages' => $messagesX,
+		// ];
 		
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		// $post = json_encode($data);
+		// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
+		// $ch = curl_init($url);
+		// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		// $result = curl_exec($ch);
+		// curl_close($ch);
 
-		echo $result . "\r\n";
-	}
+		// echo $result . "\r\n";
+	// }
 	
 }
 
