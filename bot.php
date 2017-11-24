724503fdc2b50930472b0ebad03c2a5d645a08ae
devwood <tunglersloy.c@gmail.com>
2017-11-24 09:13:28 +0700

    24

--- bot.php
+++ bot.php
@@ -163,7 +163,7 @@ if (!is_null($events['events'])) {
 			}
 			
 			
-			if(1==0)
+			if(1==1)
 			{
 				// Make a POST Request to Messaging API to reply to sender
 				$url = 'https://api.line.me/v2/bot/message/reply';
