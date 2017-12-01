a025dddf9aeb08831772b4a5d8b053f4bcd09c24
devwood <tunglersloy.c@gmail.com>
2017-12-01 17:01:14 +0700

    ห

--- tos.php
+++ tos.php
@@ -39,29 +39,29 @@ if (!is_null($events['events'])) {
 				if($numrows <= 0)
 				{
 					
-					$check_user = 'SELECT * FROM "TOS"."TOKEN"; WHERE "TOKEN" = '."'".$userX."'";
-					$result = pg_exec($dbconn, $check_user);
-					$numrows = pg_numrows($result);
-					if($numrows <= 0)
-					{
-						$messages = [
-								'type' => 'text',			
-								'text' => 'ไม่มีผู้ใช้นี้ และระบบได้เพิ่มให้แล้วกรุณาให้ admin อนุมัติ'
-								];
-								$messagesX[0] = $messages;
+					// $check_user = 'SELECT * FROM "TOS"."TOKEN"; WHERE "TOKEN" = '."'".$userX."'";
+					// $result = pg_exec($dbconn, $check_user);
+					// $numrows = pg_numrows($result);
+					// if($numrows <= 0)
+					// {
+						// $messages = [
+								// 'type' => 'text',			
+								// 'text' => 'ไม่มีผู้ใช้นี้ และระบบได้เพิ่มให้แล้วกรุณาให้ admin อนุมัติ'
+								// ];
+								// $messagesX[0] = $messages;
 								
-								$insert_newuser = 'INSERT INTO "TOS"."TOKEN"("TOKEN", "STATUS") VALUES ('."'".$userX."'".','."'"."'".')';
-								$result = pg_exec($dbconn, $insert_newuser);
-					}
-					else
-					{
-						$messages = [
-								'type' => 'text',			
-								'text' => 'ผู้ใช้ยังไม่ได้รับอณุญาติ'
-								];
-								$messagesX[0] = $messages;
-					}								
-				}
+								// $insert_newuser = 'INSERT INTO "TOS"."TOKEN"("TOKEN", "STATUS") VALUES ('."'".$userX."'".','."'"."'".')';
+								// $result = pg_exec($dbconn, $insert_newuser);
+					// }
+					// else
+					// {
+						// $messages = [
+								// 'type' => 'text',			
+								// 'text' => 'ผู้ใช้ยังไม่ได้รับอณุญาติ'
+								// ];
+								// $messagesX[0] = $messages;
+					// }								
+				// }
 				
 				
 			}
