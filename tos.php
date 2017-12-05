13c2249be55a0bc2b2cce4c8aef436cd4a3e7d90
devwood <tunglersloy.c@gmail.com>
2017-12-05 20:28:28 +0700

    3

--- tos.php
+++ tos.php
@@ -34,6 +34,36 @@ if (!is_null($events['events'])) {
 			if(1==0)
 			{
 				$s = 1;
+				
+				$check_user = 'SELECT * FROM "TOS"."TOKEN"; WHERE "STATUS"='."'"."OK"."'".' AND "TOKEN" = '."'".$userX."'";
+				// $result = pg_exec($dbconn, $check_user);
+				// $numrows = pg_numrows($result);
+				// if($numrows <= 0)
+				// {
+					
+					// $check_user = 'SELECT * FROM "TOS"."TOKEN"; WHERE "TOKEN" = '."'".$userX."'";
+					// $result = pg_exec($dbconn, $check_user);
+					// $numrows = pg_numrows($result);
+					// if($numrows <= 0)
+					// {
+						// $messages = [
+								// 'type' => 'text',			
+								// 'text' => 'ไม่มีผู้ใช้นี้ และระบบได้เพิ่มให้แล้วกรุณาให้ admin อนุมัติ'
+								// ];
+								// $messagesX[0] = $messages;
+								
+								// $insert_newuser = 'INSERT INTO "TOS"."TOKEN"("TOKEN", "STATUS") VALUES ('."'".$userX."'".','."'"."'".')';
+								// $result = pg_exec($dbconn, $insert_newuser);
+					// }
+					// else
+					// {
+						// $messages = [
+								// 'type' => 'text',			
+								// 'text' => 'ผู้ใช้ยังไม่ได้รับอณุญาติ'
+								// ];
+								// $messagesX[0] = $messages;
+					// }								
+				// }
 			}
 			else
 			{
@@ -47,7 +77,7 @@ if (!is_null($events['events'])) {
 			
 			$messages = [
 				'type' => 'text',			
-				'text' => 'R2 สอบถามวันที่ 01/01 เวลา 02:50 โดย:'.$userX
+				'text' => 'R3 สอบถามวันที่ 01/01 เวลา 02:50 โดย:'.$userX
 				];
 				$messagesX[0] = $messages;
 			
