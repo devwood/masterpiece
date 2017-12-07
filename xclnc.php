c01e0dd27f728da9e079b098942c525ad6c9026a
devwood <tunglersloy.c@gmail.com>
2017-12-07 15:20:17 +0700

    36

--- xclnc.php
+++ xclnc.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 			
 				$messages = [
 				'type' => 'text',			
-				'text' => 'FU R35 ALL POS='. $returnonline
+				'text' => 'FU R36 ALL POS='. $returnonline
 				];
 				$messagesX[0] = $messages;
 				
@@ -72,7 +72,7 @@ if (!is_null($events['events'])) {
 				
 					$messages = [
 					'type' => 'text',			
-					'text' => 'FU R35='.$returnonline
+					'text' => 'FU R36='.$returnonline
 					];
 					$messagesX[0] = $messages;
 				}
@@ -80,7 +80,7 @@ if (!is_null($events['events'])) {
 				{
 					$messages = [
 					'type' => 'text',			
-					'text' => 'FU R35 ไม่มีข้อมูล POS Online ใน 5 นาทีนี้:'.$know
+					'text' => 'FU R36 ไม่มีข้อมูล POS Online ใน 5 นาทีนี้:'.$know
 					];
 					$messagesX[0] = $messages;
 				}
@@ -91,7 +91,7 @@ if (!is_null($events['events'])) {
 			{
 				$messages = [
 				'type' => 'text',			
-				'text' => 'FU R35='.strtoupper($text)
+				'text' => 'FU R36='.strtoupper($text)
 				];
 				$messagesX[0] = $messages;
 				
@@ -164,13 +164,13 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 		
 		if($numrows_touser > 0)
 		{	
-			$ins_cmd = 'INSERT INTO public."QUERY_CMD"("FORM_TOKEN", "TO_TOKEN_CLIENT_ID", "CMD_REQUEST") VALUES ('."'".$access_token."'".', '."'".$cmd_to."'".', '."'".$cmd_str."'".');';
+			$ins_cmd = 'INSERT INTO public."QUERY_CMD"("FORM_TOKEN", "TO_TOKEN_CLIENT_ID", "CMD_REQUEST") VALUES ('."'".$userX."'".', '."'".$cmd_to."'".', '."'".$cmd_str."'".');';
 			$result_ins_cmd = pg_exec($dbconn, $ins_cmd);
 	
 			$return = pg_fetch_result($result_grp, 0, 3);
 			$messages = [
 			'type' => 'text',			
-			'text' => 'FU R35='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str
+			'text' => 'FU R36='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
@@ -180,7 +180,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 			$return = 'ไม่มีข้อมูลฐานข้อมูล '.$cmd_to;
 			$messages = [
 			'type' => 'text',			
-			'text' => 'FU R35='.$return
+			'text' => 'FU R36='.$return
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
@@ -196,7 +196,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 		$return = $userX.' ยังไม่ได้รับอณุญาติให้เข้าระบบ='.$ins_cmd;
 		$messages = [
 		'type' => 'text',			
-		'text' => 'FU R35='.$return
+		'text' => 'FU R36='.$return
 		];
 		$messagesX[0] = $messages;
 		$numrows = 1;
