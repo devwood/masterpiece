d11b5e7582329d581bcb10cf6ba3186056620295
devwood <tunglersloy.c@gmail.com>
2017-12-07 15:06:58 +0700

    33

--- xclnc.php
+++ xclnc.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 			
 				$messages = [
 				'type' => 'text',			
-				'text' => 'FU R32 ALL POS='. $returnonline
+				'text' => 'FU R33 ALL POS='. $returnonline
 				];
 				$messagesX[0] = $messages;
 				
@@ -72,7 +72,7 @@ if (!is_null($events['events'])) {
 				
 					$messages = [
 					'type' => 'text',			
-					'text' => 'FU R32='.$returnonline
+					'text' => 'FU R33='.$returnonline
 					];
 					$messagesX[0] = $messages;
 				}
@@ -80,7 +80,7 @@ if (!is_null($events['events'])) {
 				{
 					$messages = [
 					'type' => 'text',			
-					'text' => 'FU R32 ไม่มีข้อมูล POS Online ใน 5 นาทีนี้:'.$know
+					'text' => 'FU R33 ไม่มีข้อมูล POS Online ใน 5 นาทีนี้:'.$know
 					];
 					$messagesX[0] = $messages;
 				}
@@ -91,7 +91,7 @@ if (!is_null($events['events'])) {
 			{
 				$messages = [
 				'type' => 'text',			
-				'text' => 'FU R32='.strtoupper($text)
+				'text' => 'FU R33='.strtoupper($text)
 				];
 				$messagesX[0] = $messages;
 				
@@ -143,14 +143,11 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 	$result_grp = pg_exec($dbconn, $chk_access_loop);
     $numrows_grp = pg_numrows($result_grp);
 	$messagesX = array(1);
-	
+
 	if($numrows_grp > 0)
 	//if(1==1)//ใช้ไปก่อน
 	{
-		$cmd_sp = explode("XQUERY", strtoupper($text));
-		$cmd_to = $cmd_sp [0];
-		$cmd_str = $cmd_sp [1];
-		$cmd_to = trim($cmd_to);
+		
 		
 		
 		$check_user = 'SELECT * FROM public."QUERY_TOKEN" WHERE "TOKEN" = '."'".$cmd_to."'";
@@ -159,13 +156,13 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 		
 		if($numrows_touser > 0)
 		{	
-			$ins_cmd = 'INSERT INTO public."QUERY_CMD"("FORM_TOKEN", "TO_TOKEN_CLIENT_ID", "CMD_REQUEST") VALUES ('."'".$access_token."'".', '."'".$cmd_to."'".', '."'".$cmd_str."'".');';
+			$ins_cmd = 'INSERT INTO public."QUERY_CMD"("FORM_TOKEN", "TO_TOKEN_CLIENT_ID", "CMD_REQUEST") VALUES ('."'".$access_token."'".', '."'".$userX."'".', '."'".$cmd_str."'".');';
 			$result_ins_cmd = pg_exec($dbconn, $ins_cmd);
 	
 			$return = pg_fetch_result($result_grp, 0, 3);
 			$messages = [
 			'type' => 'text',			
-			'text' => 'FU R32='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str
+			'text' => 'FU R33='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
@@ -175,7 +172,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 			$return = 'ไม่มีข้อมูลฐานข้อมูล '.$cmd_to;
 			$messages = [
 			'type' => 'text',			
-			'text' => 'FU R32='.$return
+			'text' => 'FU R33='.$return
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
@@ -191,7 +188,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 		$return = $userX.' ยังไม่ได้รับอณุญาติให้เข้าระบบ='.$ins_cmd;
 		$messages = [
 		'type' => 'text',			
-		'text' => 'FU R32='.$return
+		'text' => 'FU R33='.$return
 		];
 		$messagesX[0] = $messages;
 		$numrows = 1;
