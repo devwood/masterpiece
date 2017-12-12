8963bdc0baf2e0a70ccdf863fc803a07a427ffb1
devwood <tunglersloy.c@gmail.com>
2017-12-12 11:12:26 +0700

    8

--- tos.php
+++ tos.php
@@ -64,7 +64,7 @@ if (!is_null($events['events'])) {
 						
 						$messages = [
 								'type' => 'text',			
-								'text' => 'R7 ไม่มีผู้ใช้นี้ และระบบได้เพิ่มให้แล้วกรุณาให้ admin อนุมัติ '//.$insert_newuser
+								'text' => 'R8 ไม่มีผู้ใช้นี้ และระบบได้เพิ่มให้แล้วกรุณาให้ admin อนุมัติ '//.$insert_newuser
 								];
 								$messagesX[0] = $messages;
 					}
@@ -82,7 +82,7 @@ if (!is_null($events['events'])) {
 							{
 								$messages = [
 								'type' => 'text',			
-								'text' => 'R7 กรุณาใส่ชื่อของคุณ'//.json_encode($event)
+								'text' => 'R8 กรุณาใส่ชื่อของคุณ'//.json_encode($event)
 								];
 								$messagesX[0] = $messages;
 								
@@ -96,7 +96,7 @@ if (!is_null($events['events'])) {
 								
 								$messages = [
 								'type' => 'text',			
-								'text' => 'R7 รอ Admin อนุมัติสักครู่' //.json_encode($event)
+								'text' => 'R8 รอ Admin อนุมัติสักครู่' //.json_encode($event)
 								];
 								$messagesX[0] = $messages;
 								
@@ -106,7 +106,7 @@ if (!is_null($events['events'])) {
 							{
 								$messages = [
 								'type' => 'text',			
-								'text' => 'R7 อยู่นอกลูป='.$return_cmd.' CMD='.$get_loop
+								'text' => 'R8 อยู่นอกลูป='.$return_cmd.' CMD='.$get_loop
 								];
 								$messagesX[0] = $messages;
 							}
@@ -115,7 +115,7 @@ if (!is_null($events['events'])) {
 						{
 							$messages = [
 							'type' => 'text',			
-							'text' => 'R7 ผู้ใช้ยังไม่ได้รับอณุญาติ หรือมีความผิดปกติ กรุณาติดต่อผู้ดูแลระบบ พร้อมแจ้ง Code='.$userX//.json_encode($event)
+							'text' => 'R8 ผู้ใช้ยังไม่ได้รับอณุญาติ หรือมีความผิดปกติ กรุณาติดต่อผู้ดูแลระบบ พร้อมแจ้ง Code='.$userX//.json_encode($event)
 							];
 							$messagesX[0] = $messages;
 						}
@@ -152,7 +152,7 @@ if (!is_null($events['events'])) {
 						
 						$messages = [
 						'type' => 'text',			
-						'text' => 'R7 พร้อมเริ่ม Loop ใหม่ '.pg_fetch_result($result, 0, 4).' ins_cmd='.$chk_opencmd
+						'text' => 'R8 พร้อมเริ่ม Loop ใหม่ '.pg_fetch_result($result, 0, 4).' ins_cmd='.$chk_opencmd
 						];
 						$messagesX[0] = $messages;
 					}
@@ -169,11 +169,25 @@ if (!is_null($events['events'])) {
 						$parameter_loop = pg_fetch_result($result, 0, 7);
 						$id_loop = pg_fetch_result($result, 0, 0);
 						
+						
+						
+						
+						$dataxx = [
+							'replyToken' => $replyToken,
+							'messages' => $messagesX,
+						];	
+						$postxx = json_encode($dataxx);
+
+	
+						$insert_tempx = 'INSERT INTO "TOS"."TEMPX"("TEMPX")VALUES ('."'123'".');';
+						$resultx = pg_exec($dbconn, $insert_tempx);
+
+						
 						if($parameter_loop != '')
 						{	
 							$messages = [
 							'type' => 'text',			
-							'text' => 'R7 กำลังค้นข้อมูลกรุณารอสักครู่  หรือหากนานเกินไปกรุณาแจ้งทางผู้ดูแลระบบ'."\r\n"."TEST"
+							'text' => 'R8 กำลังค้นข้อมูลกรุณารอสักครู่  หรือหากนานเกินไปกรุณาแจ้งทางผู้ดูแลระบบ'."\r\n"."TEST"
 							];
 							$messagesX[0] = $messages;
 						}
@@ -186,7 +200,7 @@ if (!is_null($events['events'])) {
 							
 							$messages = [
 							'type' => 'text',			
-							'text' => 'R7 กำลังค้นข้อมูลกรุณารอสักครู่ '
+							'text' => 'R8 กำลังค้นข้อมูลกรุณารอสักครู่ '
 							];
 							$messagesX[0] = $messages;
 						}
@@ -200,7 +214,7 @@ if (!is_null($events['events'])) {
 					// $return_user = pg_fetch_result($result, 0, 3);
 					// $messages = [
 					// 'type' => 'text',			
-					// 'text' => 'R7 สวัสดี '.$return_user.' ที่คุณสอบถามไม่อยู่ใน Scope การใช้งานของคุณ'
+					// 'text' => 'R8 สวัสดี '.$return_user.' ที่คุณสอบถามไม่อยู่ใน Scope การใช้งานของคุณ'
 					// ];
 					// $messagesX[0] = $messages;
 				}
@@ -238,8 +252,8 @@ function _sendOut($access_token, $replyToken, $messagesX)
 	$post = json_encode($data);
 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 	
-	$insert_tempx = 'INSERT INTO "TOS"."TEMPX"("TEMPX")VALUES ('."'123'".');';
-	$result = pg_exec($dbconn, $insert_tempx);
+	// $insert_tempx = 'INSERT INTO "TOS"."TEMPX"("TEMPX")VALUES ('."'123'".');';
+	// $result = pg_exec($dbconn, $insert_tempx);
 
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
