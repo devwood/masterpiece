d6852735d7e92b89d33db3c76f78e670e7045515
devwood <tunglersloy.c@gmail.com>
2018-01-18 13:08:14 +0700

    12

--- xclnc.php
+++ xclnc.php
@@ -183,7 +183,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 			$return = pg_fetch_result($result_grp, 0, 3);
 			$messages = [
 			'type' => 'text',			
-			'text' => '4 R12='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str." exp=".$expand;//.'ins='.$ins_cmd
+			'text' => '4 R12='.$return." ไปยัง ".$cmd_to." ด้วยคำสั่ง ".$cmd_str." exp=".$expand//.'ins='.$ins_cmd
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
