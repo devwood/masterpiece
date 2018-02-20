930cc11df3dccdf3b986278b4cce24e2ce55490e
devwood <tunglersloy.c@gmail.com>
2018-02-20 18:02:47 +0700

    6

--- xclnc.php
+++ xclnc.php
@@ -219,7 +219,7 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 		
 		
 		
-		$check_user = 'SELECT * FROM public."QUERY_TOKEN" WHERE UPPER("TOKEN") = '."'".$cmd_to."'";
+		$check_user = 'SELECT * FROM public."QUERY_TOKEN" WHERE "TOKEN" = '."'".$cmd_to."'";
 		$result_touser = pg_exec($dbconn, $check_user);
 		$numrows_touser = pg_numrows($result_touser);
 		
@@ -245,14 +245,14 @@ function _resultXQUERY($text, $dbconn, $event, $access_token)
 			$return = pg_fetch_result($result_grp, 0, 3);
 			$messages = [
 			'type' => 'text',			
-			'text' => 'R7 ทำการเรียกข้อมูล '//.$check_user
+			'text' => 'R6 ทำการเรียกข้อมูล '//.$check_user
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
 		}
 		else
 		{
-			$return = 'R7 ไม่มีข้อมูลฐานข้อมูล '.$check_user;
+			$return = 'R6 ไม่มีข้อมูลฐานข้อมูล '.$check_user;
 			$messages = [
 			'type' => 'text',			
 			'text' => $return
@@ -361,7 +361,7 @@ function _setINI($text, $dbconn, $event, $access_token)
 			$return = pg_fetch_result($result_grp, 0, 3);
 			$messages = [
 			'type' => 'text',			
-			'text' => 'R7 กำลัง Set INI'
+			'text' => 'R6 กำลัง Set INI'
 			];
 			$messagesX[0] = $messages;
 			$numrows = 1;
