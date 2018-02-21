3652955d7b1a01f237cc538015d7ded38c1ffa5e
devwood <tunglersloy.c@gmail.com>
2018-02-21 15:28:39 +0700

    svr

---
+++ xclnc_run_tool_srv.php
@@ -0,0 +1,41 @@
+<?php
+$jobid=$_GET["job"];
+$dbconn = pg_connect("host=ec2-107-22-252-91.compute-1.amazonaws.com port=5432 dbname=d1t089mnl00iir user=feajajzganbfiq password=57ba34efa8018b168b1edbdd5849b55f67c2a8a1f48e644a1e1fc6e951d9517a");
+
+// // $servername = "163.44.198.39";
+// // $username = "cp572795";
+// // $password = "1022@Mine";
+// // $dbx = "cp572795_LINEQUERY_TOS";
+// // $conn = new mysqli($servername, $username, $password, $dbx);
+// // mysqli_set_charset($conn,"utf8");
+
+// // $check_job = "select * from JOB_DETAIL where SALE_ORDER = '".$jobid."'";
+// // $result = $conn->query($check_job);	
+
+// // $rows = array();
+// // while($r = mysqli_fetch_assoc($result)) {
+    // // $rows[] = $r;
+// // }
+
+// // $myJSON = json_encode($rows);
+
+
+
+
+// $check_module = 'SELECT DISTINCT Q."QUESTION_DESC", Q."NOTE"
+										// FROM "TOS"."QUESTION_TYPEvsTOKEN" QT
+										// INNER JOIN "TOS"."QUESTION_TYPE" Q ON QT."QUESTION_TYPE_ID" = Q."ID"
+										// INNER JOIN "TOS"."TOKEN" T ON QT."TOKEN_ID" = T."ID"';
+// $result = pg_exec($dbconn, $check_module);
+
+// $rows = array();
+// while($r = pg_fetch_row($result)) {
+    // $rows[] = $r;
+// }
+
+
+// $myJSON = json_encode($rows );
+
+// echo $myJSON;
+echo '55';
+?>
