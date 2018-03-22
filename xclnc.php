4f2b4aaf20a3efade297b376abc0137fd376545d
devwood <tunglersloy.c@gmail.com>
2018-03-22 10:49:15 +0700

    R2

--- xclnc.php
+++ xclnc.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 					
 					$messages = [
 						'type' => 'text',			
-						'text' => 'ชื่อคุณคือ'.$text.' '.strpos('I AM TAE')
+						'text' => 'R2 ชื่อคุณคือ'.$text.' '.strpos('I AM','I AM TAE')
 						];
 						$messagesX[0] = $messages;
 						
